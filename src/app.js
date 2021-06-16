b188117027b09dc4a4583a1c8bb2b7121abe38e0
alexkuo40929 <alexkuo40929@gmail.com>
2021-06-15 17:13:59 -0700

    JS forecast data

--- src/app.js
+++ src/app.js
@@ -42,6 +42,25 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["thu", "fri", "sat"];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `
+                        <div class="col-2">
+                            <div class="weather-forecast-date">${day}</div>
+                            <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="" width="42">
+                            <div class="weather-forecast-temperatures">
+                                <span class="weather-forecast-temperature-max">18° </span>
+                                <span class="weather-forecast-temperature-min">12°</span>
+                            </div>
+                    </div>`;
+    })
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function handleSubmit(event) {
     event.preventDefault();
     let cityInputElement = document.querySelector("#city-input");
@@ -73,6 +92,8 @@ function displayFahrenheitTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+displayForecast();
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
